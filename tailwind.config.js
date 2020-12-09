9f93ff560532e71074477b686c73affaecb095e2
David East <deast@google.com>
2020-12-09 08:39:38 -0500

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   purge: {
     enabled: isProduction,
@@ -8,10 +9,24 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      white: "hsl(227 2% 94%)",
+      black: "hsl(227 94% 4%)",
+      green: "hsl(140 100% 20%)",
+    },
+    fontFamily: {
+      body: ["'Karla'", "sans-serif"],
+      display: ["'Arvo'", "sans-serif"],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    // It's a fixed grid so we don't need a container
+    // TODO(davideast): is this needed b/c of the css purge?
+   container: false,
+  },
 }
