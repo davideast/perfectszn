41181c7fb018cccfc2ce5f440793a74be9af7211
David East <davideast@users.noreply.github.com>
2020-12-21 03:32:39 +0000

    actually load

--- src/js/firebase.ts
+++ src/js/firebase.ts
@@ -1,5 +1,7 @@
 import firebase from 'firebase/app';
 import config from './firebaseConfig';
+import 'firebase/analytics';
+import 'firebase/performance';
 
 function loadFirebase(): firebase.app.App {
   const firebaseApp = firebase.initializeApp(config);
