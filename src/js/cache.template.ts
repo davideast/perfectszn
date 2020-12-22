9ed86192d7d238fd0ac9d59fc156489b609c1ec1
David East <davideast@users.noreply.github.com>
2020-12-22 16:51:25 +0000

    cache

--- src/js/cache.template.ts
+++ src/js/cache.template.ts
@@ -8,8 +8,6 @@ if(cacheAvailable) {
 async function getCache() {
   const cache = await caches.open('szn-cache-::version::');
   requests.forEach(request => {
-    if(!cache.match(request)) {
-      cache.add(new Request(request));
-    }
+    cache.add(new Request(request));
   });
 }
